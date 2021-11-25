66f9e0d6eaddb93b8b7d139ee2b6f15e4b901565
simopan93 <diegobuonanotte10@gmail.com>
2021-11-25 17:05:01 +0100

    esercizio finito

---
+++ src/assets/data/ListSocial.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    social:"Facebok",
+    logo: require("../img/footer-facebook.png"),
+  },
+  {
+    social:"Twitter",
+    logo:require("../img/footer-twitter.png"),
+  },
+  {
+    social:"Youtube",
+    logo:require("../img/footer-youtube.png"),
+  },
+  {
+    social:"Pinterest",
+    logo:require("../img/footer-pinterest.png"),
+  },
+  {
+    social:"Periscope",
+    logo:require("../img/footer-periscope.png"),
+  }
+]
--- src/components/BottomFooter.vue
+++ src/components/BottomFooter.vue
@@ -1,19 +1,112 @@
 <template>
   <section class="bottom-footer">
+    <div class="container">
+
+      <button>SIGN-UP NOW</button>
+
+      <div class="box-social">
+        <a>FOLLOW US</a>
+        <div v-for="(item,index) in Social"
+        :key="index" 
+        class="box-logo">
+
+          <img :src="item.logo" alt="item.social">
+        
+        </div>
+
+      </div>
+    </div>
+    
 
   </section>
   
 </template>
 
+
+
+
+
 <script>
+import Social from "../assets/data/ListSocial.js"
+
+
+
 export default {
-   name: "BottomFooter"
+   name: "BottomFooter",
+
+
+    data() {
+      return{
+        Social
+      }
+    }
 }
 </script>
 
+
+
+
+
 <style  lang="scss">
+  @import "../assets/styles/vars.scss";
+  @import "../assets/styles/mixins.scss";
+
+
+
+
   .bottom-footer {
     height: 80px;
     background-color: #303030;
+
+    .container{
+      @include center(align);
+      justify-content: space-between;
+      height: 100%;
+
+
+
+      button {
+        color: white;
+        font-size: 15px;
+        border: 2px solid $primary-color;
+        background-color: #303030;
+        padding: 10px;
+
+        &:hover{
+          cursor: pointer;
+          color: lightgray;
+        }
+      }
+
+      .box-social{
+        @include center();
+
+
+        a {
+          color: $primary-color;
+          margin-right: 15px;
+
+
+          &:hover {
+            cursor: pointer;
+          }
+        }
+
+        img {
+          margin: 0 10px;
+          transition: background-color 0.2s;
+          transition: border-radius 0s;
+
+
+          &:hover {
+            cursor: pointer;
+            background-color: white;
+            border-radius: 50%;
+          }
+        }
+      }
+    }
+
+    
   }
 </style>
